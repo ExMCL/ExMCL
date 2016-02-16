67fe5ed17f40d3fd717f64e26efeffda74cb9cf6
n9Mtq4 <bill.wresnahan@gmail.com>
2016-02-15 21:08:40 -0500

    the tabs use the default laf

--- ExMCL API/src/main/kotlin/com/n9mtq4/exmcl/api/tabs/ui/TabTab.kt
+++ ExMCL API/src/main/kotlin/com/n9mtq4/exmcl/api/tabs/ui/TabTab.kt
@@ -1,6 +1,7 @@
 package com.n9mtq4.exmcl.api.tabs.ui
 
 import javax.swing.JTabbedPane
+import javax.swing.UIManager
 
 /**
  * Created by will on 2/13/16 at 12:35 AM.
@@ -10,6 +11,13 @@ import javax.swing.JTabbedPane
 class TabTab : JTabbedPane() {
 	
 	init {
+		
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		}catch (e: Exception) {
+			e.printStackTrace()
+		}
+		
 		tabLayoutPolicy = JTabbedPane.WRAP_TAB_LAYOUT
 	}
 	
