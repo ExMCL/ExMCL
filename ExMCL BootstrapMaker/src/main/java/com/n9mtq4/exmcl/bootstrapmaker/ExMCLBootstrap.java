6d7176e2b3ee1fb146a49370cff668ac36cca95b
n9Mtq4 <bill.wresnahan@gmail.com>
2016-04-23 22:25:20 -0400

    mit license

--- ExMCL BootstrapMaker/src/main/java/com/n9mtq4/exmcl/bootstrapmaker/ExMCLBootstrap.java
+++ ExMCL BootstrapMaker/src/main/java/com/n9mtq4/exmcl/bootstrapmaker/ExMCLBootstrap.java
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2016 Will (n9Mtq4) Bresnahan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.n9mtq4.exmcl.bootstrapmaker;
 
 import com.n9mtq4.logwindow.BaseConsole;
@@ -15,7 +39,11 @@ import java.net.Proxy;
 
 /**
  * Created by will on 1/6/16 at 5:26 PM.
- *
+ * 
+ * NOTE: THIS CODE WAS BASED OFF OF WHAT MOJANG WROTE.
+ * THIS CODE DOESN'T INCLUDE MUCH OF THAT CODE
+ * AND SIGNIFICANTLY CHANGES THE METHOD IT DOES CHANGE
+ * 
  * @author Will "n9Mtq4" Bresnahan
  */
 public final class ExMCLBootstrap extends Bootstrap {
