640b21e66a156ec4afe4b7e6be0e79cb33869663
n9Mtq4 <bill.wresnahan@gmail.com>
2016-09-12 17:18:51 -0400

    gradle 3.0

--- ExMCL API/src/main/kotlin/com/n9mtq4/exmcl/api/Version.kt
+++ ExMCL API/src/main/kotlin/com/n9mtq4/exmcl/api/Version.kt
@@ -1,5 +1,5 @@
 /*AUTO GENERATED FILE*/
 @file:JvmName("Version")
 package com.n9mtq4.exmcl.api
-const val BUILD_NUMBER = 549
-const val TIME_STAMP = 1468647555412L
+const val BUILD_NUMBER = 573
+const val TIME_STAMP = 1473714781531L
