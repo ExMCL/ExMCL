4dad612c259938560842c224aaefd6c1e677736f
n9Mtq4 <bill.wresnahan@gmail.com>
2016-11-26 16:01:25 -0500

    moved psd out of build

--- ExMCL API/src/main/kotlin/com/n9mtq4/exmcl/api/Version.kt
+++ ExMCL API/src/main/kotlin/com/n9mtq4/exmcl/api/Version.kt
@@ -1,5 +1,5 @@
 /*AUTO GENERATED FILE*/
 @file:JvmName("Version")
 package com.n9mtq4.exmcl.api
-const val BUILD_NUMBER = 639
-const val TIME_STAMP = 1480190113049L
+const val BUILD_NUMBER = 643
+const val TIME_STAMP = 1480193291671L
